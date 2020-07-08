c879226be5aef2b8e035580c3c5dd6e846f8b211
Renan Azzolim <azzolimrenan2@gmail.com>
2020-07-07 22:43:51 -0300

    Fallback, circuit breaker + bulkhead (hystrix)

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -5,11 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import br.com.alura.microservice.loja.client.FornecedorClient;
 import br.com.alura.microservice.loja.dto.CompraDTO;
 import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
 import br.com.alura.microservice.loja.dto.InfoPedidoDTO;
 import br.com.alura.microservice.loja.model.Compra;
+import br.com.alura.microservice.loja.repository.CompraRepository;
 
 @Service
 public class CompraService {
@@ -18,12 +21,22 @@ public class CompraService {
 
 	@Autowired
 	private FornecedorClient fornecedorClient;
+	
+	@Autowired
+	private CompraRepository compraRepository;
 
+	@HystrixCommand(threadPoolKey = "getByIdThreadPoolKey")
+	public Compra getById(Long id) {
+		return compraRepository.findById(id).orElse(new Compra());
+	}
+
+	@HystrixCommand(fallbackMethod = "realizaCompraFallback", threadPoolKey = "realizaCompraThreadPool")
 	public Compra realizaCompra(CompraDTO compra) {
 		final String estado = compra.getEndereco().getEstado();
 		
 		LOGGER.info("buscando informações do fornecedor de {}", estado);
 		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(estado);
+		System.out.println(info.getEndereco());
 		
 		LOGGER.info("realizando um pedido");
 		InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
@@ -32,9 +45,16 @@ public class CompraService {
 		compraSalva.setPedidoId(pedido.getId());
 		compraSalva.setTempoDePreparo(pedido.getTempoDePreparo());
 		compraSalva.setEnderecoDestino(compra.getEndereco().toString());
+		compraRepository.save(compraSalva);
 		
 		return compraSalva;
 
+	}
+	
+	public Compra realizaCompraFallback(CompraDTO compra) {
+		Compra compraFallback = new Compra();
+		compraFallback.setEnderecoDestino(compra.getEndereco().toString());
+		return compraFallback;
 	}
 
 }
